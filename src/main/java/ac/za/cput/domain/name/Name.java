b5e8f665aa785c95c07c3a126885064e1da1dcae
Anicka Schouw <anickaschouw@gmail.com>
2022-06-18 18:02:39 +0200

    Name Domain created, @author: Anicka Schouw 217284183

--- src/main/java/ac/za/cput/domain/name/Name.java
+++ src/main/java/ac/za/cput/domain/name/Name.java
@@ -1,11 +1,21 @@
 package ac.za.cput.domain.name;
-
+/*
+Name.java
+This is the domain class for Name
+@author: Anicka Schouw 217284183
+June 2022
+ */
+import com.sun.istack.NotNull;
 import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import java.io.Serializable;
 
+@Entity
 @Embeddable
-public class Name {
-    public String  firstName;
+public class Name implements Serializable {
+   @NotNull public String  firstName;
     public String middleName;
+    @NotNull public String lastName;
     protected Name(){}
 
 
@@ -15,31 +25,23 @@ public class Name {
         this.lastName = lastName;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public Name(Builder builder){
+        this.firstName = builder.firstName;
+        this.middleName = builder.middleName;
+        this.lastName = builder.lastName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public String getFirstName() {
+        return firstName;
     }
 
     public String getMiddleName() {
         return middleName;
     }
 
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
     public String getLastName() {
         return lastName;
     }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String lastName;
 
     @Override
     public String toString() {
@@ -48,6 +50,37 @@ public class Name {
                 ", middleName='" + middleName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 '}';
+    }
+    public static class Builder{
+        private String firstName;
+        private String middleName;
+        private String lastName;
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setMiddleName(String middleName) {
+            this.middleName = middleName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder copy(Name name){
+            this.firstName = name.firstName;
+            this.middleName = name.middleName;
+            this.lastName = name.lastName;
+            return this;
+        }
+
+        public Name build(){
+            return new Name(this);
+        }
     }
 
 }
