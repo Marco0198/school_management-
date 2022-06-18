a82e985079dd0c58361237c5be77873edf9f6945
Marco0198 <81382003+Marco0198@users.noreply.github.com>
2022-06-18 09:49:45 +0200

    factory class for employee

--- src/main/java/ac/za/cput/factory/employee/EmployeeFactory.java
+++ src/main/java/ac/za/cput/factory/employee/EmployeeFactory.java
@@ -1,4 +1,18 @@
 package ac.za.cput.factory.employee;
 
+import ac.za.cput.domain.employee.Employee;
+import ac.za.cput.domain.name.Name;
+import ac.za.cput.helper.StringHelper;
+
 public class EmployeeFactory {
+    public static Employee createEmployee(String staffId, String email, Name name) {
+        StringHelper.checkStringParam("staffId", staffId);
+        StringHelper.checkStringParam("email", email);
+        StringHelper.checkStringParam("firstname", name.firstName);
+        StringHelper.checkStringParam("lastname", name.lastName);
+        StringHelper.checkStringParam("middle name", name.middleName);
+
+
+        return new Employee.Builder().setStaffId(staffId).setEmail(email).setName(name).build();
+    }
 }
