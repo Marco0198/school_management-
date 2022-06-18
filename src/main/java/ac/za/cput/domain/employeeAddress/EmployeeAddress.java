1e8dc8066bd3577493c223e1574d011d7833b6b8
DamianDuToit <damianacarstens@gmail.com>
2022-06-18 20:58:12 +0200

    Added EmployeeAddress Entity

---
+++ src/main/java/ac/za/cput/domain/employeeAddress/EmployeeAddress.java
@@ -0,0 +1,71 @@
+package ac.za.cput.domain.employeeAddress;
+
+import ac.za.cput.domain.address.Address;
+import com.sun.istack.NotNull;
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import java.io.Serializable;
+/*
+ * Student Name: Damian du Toit
+ * Student Number: 219200203
+ * EmployeeAddress.java
+ * Date : 13th June 2022
+ */
+@Entity
+@Embeddable
+public class EmployeeAddress implements Serializable {
+    @NotNull
+    public String staffId;
+    @Embedded
+    @NotNull
+    public Address address;
+
+    protected EmployeeAddress(){}
+
+    private EmployeeAddress(Builder builder) {
+        this.staffId = builder.staffId;
+        this.address = builder.address;
+    }
+
+    public String getStaffId() {
+        return staffId;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeAddress{" +
+                "staffId='" + staffId + '\'' +
+                ", address=" + address +
+                '}';
+    }
+
+    public static class Builder {
+        public String staffId;
+        public Address address;
+
+        public Builder setStaffId(String staffId) {
+            this.staffId = staffId;
+            return this;
+        }
+
+        public Builder setAddress(Address address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder copy(EmployeeAddress employeeAddress) {
+            this.staffId = employeeAddress.staffId;
+            this.address = employeeAddress.address;
+            return this;
+        }
+
+        public EmployeeAddress build() {
+            return new EmployeeAddress(this);
+        }
+    }
+}
